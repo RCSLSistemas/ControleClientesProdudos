36692fcacc227c4423954f1535ff780d2a9c7839
betocarleite@yahoo.com.br <betocarleite@yahoo.com.br>
2024-11-18 23:55:59 -0300

    #

--- ControleClientesProdudos/Models/Vendas.cs
+++ ControleClientesProdudos/Models/Vendas.cs
@@ -18,7 +18,7 @@ namespace ControleClientesProdudos.Models
         private string nomeCliente;
         private int idProduto;
         private string nomeProduto;
-        private Double valorTotal;
+        private decimal valorTotal;
 
         public int IdVenda { get => idVenda; set => idVenda = value; }
         public int QtdItens { get => qtdItens; set => qtdItens = value; }
@@ -28,7 +28,7 @@ namespace ControleClientesProdudos.Models
 
         public int IdProduto { get => idProduto; set => idProduto = value; }
         public string NomeProduto { get => nomeProduto; set => nomeProduto = value; }
-        public Double ValorTotal { get => valorTotal; set => valorTotal = value; }
+        public decimal ValorTotal { get => valorTotal; set => valorTotal = value; }
 
         /// <summary>
         /// Rotina para Inserir dados
